40e53b1f84c9c3fbafe69c6e0cab25642ab9732a
Teddy Minaya <teddyminaya@gmail.com>
2022-12-12 19:34:14 -0500

    commit tree bb76b0e256cecb265042d64fe1aa7962da80c863

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(move="go to the office") {
+    return `This Monday, I will ${move}.`
+  }
+function wrapAdjective(flare="*"){
+  return function message(adj="special"){
+    return "You are "+flare+adj+flare+"!"
+  }
+}
